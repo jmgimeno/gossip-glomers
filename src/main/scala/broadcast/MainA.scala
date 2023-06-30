451cc84f867a9ef7b8936ee5d20e670d9c97b86e
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2023-06-30 08:30:02 +0200

    small refactoring of single-node broadcast

--- src/main/scala/broadcast/MainA.scala
+++ src/main/scala/broadcast/MainA.scala
@@ -6,47 +6,45 @@ import zio.json.*
 import com.bilalfazlani.zioMaelstrom.*
 import com.bilalfazlani.zioMaelstrom.protocol.*
 
-@jsonDiscriminator("type")
-enum Incoming extends NeedsReply derives JsonDecoder {
-
-  @jsonHint("topology") case Topology(
-      msg_id: MessageId,
-      topology: Map[NodeId, List[NodeId]]
-  )
-
-  @jsonHint("broadcast") case Broadcast(msg_id: MessageId, message: Int)
-
-  @jsonHint("read") case Read(msg_id: MessageId)
-}
-
-case class BroadcastOk(in_reply_to: MessageId, `type`: String = "broadcast_ok")
-    extends Sendable,
-      Reply derives JsonEncoder
-
-case class ReadOk(
-    messages: Set[Int],
-    in_reply_to: MessageId,
-    `type`: String = "read_ok"
-) extends Sendable,
-      Reply
-    derives JsonEncoder
-
-case class TopologyOk(in_reply_to: MessageId, `type`: String = "topology_ok")
-    extends Sendable,
-      Reply derives JsonEncoder
-
-case class State(messages: Set[Int]) {
-  def add(message: Int) = copy(messages = messages + message)
-}
-
-object State {
-  val live     = ZLayer.fromZIO(Ref.make(State(Set.empty)))
-  val messages = ZIO.serviceWithZIO[Ref[State]](_.get.map(_.messages))
-  def add(message: Int) =
-    ZIO.serviceWithZIO[Ref[State]](_.update(_.add(message)))
-}
-
 object MainA extends ZIOAppDefault {
+
+  @jsonDiscriminator("type")
+  enum Incoming extends NeedsReply derives JsonDecoder {
+
+    @jsonHint("topology") case Topology(
+        msg_id: MessageId,
+        topology: Map[NodeId, List[NodeId]]
+    )
+
+    @jsonHint("broadcast") case Broadcast(msg_id: MessageId, message: Int)
+
+    @jsonHint("read") case Read(msg_id: MessageId)
+  }
+
+  case class BroadcastOk(in_reply_to: MessageId, `type`: String = "broadcast_ok") extends Sendable, Reply
+      derives JsonEncoder
+
+  case class ReadOk(
+      messages: Set[Int],
+      in_reply_to: MessageId,
+      `type`: String = "read_ok"
+  ) extends Sendable,
+        Reply
+      derives JsonEncoder
+
+  case class TopologyOk(in_reply_to: MessageId, `type`: String = "topology_ok") extends Sendable, Reply
+      derives JsonEncoder
+
+  case class State(messages: Set[Int]) {
+    def add(message: Int) = copy(messages = messages + message)
+  }
+
+  object State {
+    val live = ZLayer.fromZIO(Ref.make(State(Set.empty)))
+    val messages = ZIO.serviceWithZIO[Ref[State]](_.get.map(_.messages))
+    def add(message: Int) =
+      ZIO.serviceWithZIO[Ref[State]](_.update(_.add(message)))
+  }
 
   import Incoming.*
 
@@ -59,7 +57,7 @@ object MainA extends ZIOAppDefault {
     case Read(msg_id) =>
       for {
         messages <- State.messages
-        _        <- reply(ReadOk(in_reply_to = msg_id, messages = messages))
+        _ <- reply(ReadOk(in_reply_to = msg_id, messages = messages))
       } yield ()
     case Topology(msg_id, _) =>
       for {
